849ab7dab7a36615b6dd6f9452a3c8b0a332a3d7
iFlyNyx <iFlyNick.dev@gmail.com>
2026-09-13 23:54:59 -0700

    updated for img on obs overlay to be before text instead of appended after

--- Neon.Obs.BrowserSource.WebApp/wwwroot/js/overlays/skyye.js
+++ Neon.Obs.BrowserSource.WebApp/wwwroot/js/overlays/skyye.js
@@ -196,7 +196,7 @@ function buildNewChatMessage(username, style, message, chatterFlags) {
         let broadcasterImg = document.createElement('img')
         broadcasterImg.classList.add('pe-1');
         broadcasterImg.src = "https://static-cdn.jtvnw.net/badges/v1/5527c58c-fb7d-422d-b71b-f309dcb85cc1/1";
-        neonUsername.appendChild(broadcasterImg);
+        neonUsername.prepend(broadcasterImg);
     }
     
     let chatterImageBonus = (style === 'vip' || (customUsers.includes(username.toLowerCase()) && chatterFlags.isVip)) ? document.createElement('div') : undefined;
